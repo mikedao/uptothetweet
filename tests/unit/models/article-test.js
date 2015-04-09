eafdd28d7a770b3d1d48c4a15cf2857516e5060e
Orion Osborn <orion.osborn@gmail.com>
2015-04-09 17:17:19 -0600

    add article unit test and bootstrap

--- tests/unit/models/article-test.js
+++ tests/unit/models/article-test.js
@@ -13,3 +13,13 @@ test('it exists', function(assert) {
   // var store = this.store();
   assert.ok(!!model);
 });
+
+test('it has a title and abstract', function(assert) {
+  var article = this.subject({
+    title: 'test title',
+    abstract: 'test abstract'
+  });
+
+  assert.strictEqual(article.get('title'), 'test title');
+  assert.strictEqual(article.get('abstract'), 'test abstract');
+});
