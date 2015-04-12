4bd7a937fb8acd797a867a0f11aa8915a615390c
Brandon Mrsny <bmrsny@gmail.com>
2015-04-11 18:11:13 -0600

    started adding a route for single article show page

--- app/router.js
+++ app/router.js
@@ -6,5 +6,7 @@ var Router = Ember.Router.extend({
 });
 
 export default Router.map(function() {
-  this.resource('articles', { path: '/' }, function() {});
+  this.resource('articles', { path: '/' }, function() {
+    this.resource('article', { path: ':article_id' }, function() {});
+  });
 });
