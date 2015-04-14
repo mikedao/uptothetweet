7d2faf5463b168b6a08c0301c314aecc9ed5e446
Bryce Holcomb <bryce_holcomb@outlook.com>
2015-04-13 21:10:07 -0600

    call set markers on load

--- app/components/article-map.js
+++ app/components/article-map.js
@@ -8,11 +8,12 @@ export default Ember.Component.extend({
     L.mapbox.accessToken = "pk.eyJ1IjoiYm1yc255IiwiYSI6IjlvWUIxYmcifQ.8iPZd41vr5xtnuenlkWwtw";
     // Replace "examples.map-i87786ca" with your map id.
     let mapboxTiles = L.tileLayer("https://{s}.tiles.mapbox.com/v4/examples.map-i87786ca/{z}/{x}/{y}.png?access_token=" + L.mapbox.accessToken, {
-      attribution: "<a href="http://www.mapbox.com/about/maps/" target="_blank">Terms &amp; Feedback</a>"
+      attribution: '<a href="http://www.mapbox.com/about/maps/" target="_blank">Terms &amp; Feedback</a>'
     });
 
     this.set("map", L.map(this.elementId).addLayer(mapboxTiles));
     this.setMapCenter();
+    this.setMarkers();
   }.on("didInsertElement"),
 
   setMapCenter: function () {
