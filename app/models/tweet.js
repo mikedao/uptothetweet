084268f567c0a9a5d8bb813f6eda20759421f43b
Orion Osborn <orion.osborn@gmail.com>
2015-04-14 13:15:15 -0600

    refactoring tweets model and route

--- app/models/tweet.js
+++ app/models/tweet.js
@@ -4,7 +4,7 @@ let Tweet =  DS.Model.extend({
   body: DS.attr('string'),
   username: DS.attr('string'),
   userProfileImage: DS.attr('string'),
-  article_id: DS.belongsTo('article'),
+  article: DS.belongsTo('article'),
   latitude: DS.attr('number'),
   longitude: DS.attr('number'),
   tweetId: DS.attr('string')
