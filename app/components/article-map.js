4f54cb796beab3a3de22f57c98e74bfd1448a712
Bryce Holcomb <bryce_holcomb@outlook.com>
2015-04-13 21:06:40 -0600

    change to double quotes

--- app/components/article-map.js
+++ app/components/article-map.js
@@ -1,27 +1,27 @@
-import Ember from 'ember';
+import Ember from "ember";
 
 export default Ember.Component.extend({
 
-  classNames: ['article-map'],
+  classNames: ["article-map"],
 
   buildMap: function () {
-    L.mapbox.accessToken = 'pk.eyJ1IjoiYm1yc255IiwiYSI6IjlvWUIxYmcifQ.8iPZd41vr5xtnuenlkWwtw';
-    // Replace 'examples.map-i87786ca' with your map id.
-    let mapboxTiles = L.tileLayer('https://{s}.tiles.mapbox.com/v4/examples.map-i87786ca/{z}/{x}/{y}.png?access_token=' + L.mapbox.accessToken, {
-      attribution: '<a href="http://www.mapbox.com/about/maps/" target="_blank">Terms &amp; Feedback</a>'
+    L.mapbox.accessToken = "pk.eyJ1IjoiYm1yc255IiwiYSI6IjlvWUIxYmcifQ.8iPZd41vr5xtnuenlkWwtw";
+    // Replace "examples.map-i87786ca" with your map id.
+    let mapboxTiles = L.tileLayer("https://{s}.tiles.mapbox.com/v4/examples.map-i87786ca/{z}/{x}/{y}.png?access_token=" + L.mapbox.accessToken, {
+      attribution: "<a href="http://www.mapbox.com/about/maps/" target="_blank">Terms &amp; Feedback</a>"
     });
 
-    this.set('map', L.map(this.elementId).addLayer(mapboxTiles));
+    this.set("map", L.map(this.elementId).addLayer(mapboxTiles));
     this.setMapCenter();
-  }.on('didInsertElement'),
+  }.on("didInsertElement"),
 
   setMapCenter: function () {
-    if (this.get('article.latitude') && this.get('article.longitude')) {
-      this.get('map').setView([this.get('article.latitude'), this.get('article.longitude')], 10);
+    if (this.get("article.latitude") && this.get("article.longitude")) {
+      this.get("map").setView([this.get("article.latitude"), this.get("article.longitude")], 10);
     } else {
-      this.get('map').setView([0, 0], 1);
+      this.get("map").setView([0, 0], 1);
     }
-  }.observes('article'),
+  }.observes("article"),
 
   setMarkers: function() {
     var tweets = this.get("article.tweets");
@@ -42,9 +42,9 @@ export default Ember.Component.extend({
           properties: {
             title: tweet.get("username"),
             description: tweet.get("body"),
-            'marker-size': 'large',
-            'marker-color': '#BE9A6B',
-            'marker-symbol': 'circle'
+            "marker-size": "large",
+            "marker-color": "#BE9A6B",
+            "marker-symbol": "circle"
           }
         }
 
